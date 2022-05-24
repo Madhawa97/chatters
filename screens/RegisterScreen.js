07fd393e860ee2d07d2fe43861ae215b1b8a372e
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-05-24 13:32:27 +0530

    Change default profile image url

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -18,7 +18,7 @@ const RegisterScreen = ({ navigation }) => {
 
                 user.updateProfile({
                     displayName: name,
-                    photoURL: imageURL ? imageURL : "https://example.com/jane-q-user/profile.jpg"
+                    photoURL: imageURL ? imageURL : "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0="
                 }).then(() => {
                     // Update successful
                     // ...
