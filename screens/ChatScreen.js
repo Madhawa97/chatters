4f0715d5a2a1d8ea8cb43b01da324298c0a7a0cf
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-05-24 12:31:01 +0530

    Add signout function, button and Avatar

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { auth } from '../firebase';
+import { AntDesign } from '@expo/vector-icons';
+import { Avatar } from '@rneui/base';
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation }) => {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <View style={{ marginLeft: 20 }}>
+                    <Avatar
+                        rounded
+                        source={{
+                            uri: auth?.currentUser?.photoURL
+                        }}
+                    />
+                </View>
+            ),
+            headerRight: () => (
+                <TouchableOpacity style={{ marginRight: 30 }} onPress={signOut}>
+                    <AntDesign name="logout" size={24} color="black" />
+                </TouchableOpacity>
+            )
+        })
+    }, [])
+
+    const signOut = () => {
+        auth.signOut().then(() => {
+            navigation.replace('Login');
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
     return (
         <View>
             <Text>ChatScreen</Text>
