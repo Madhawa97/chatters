8fce4b5e502def0043c3f0ea752ead95d2166072
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-05-24 12:52:16 +0530

    Add GiftedChat to chatscreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,34 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState, useCallback, useEffect } from 'react'
 import { auth } from '../firebase';
 import { AntDesign } from '@expo/vector-icons';
 import { Avatar } from '@rneui/base';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 const ChatScreen = ({ navigation }) => {
+    // gifted chat code from 'https://github.com/FaridSafi/react-native-gifted-chat#:~:text=const%20%5Bmessages%2C%20setMessages%5D%20%3D,))%0A%20%20%7D%2C%20%5B%5D)'
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    
     useLayoutEffect(() => {
         navigation.setOptions({
             headerLeft: () => (
@@ -33,9 +57,14 @@ const ChatScreen = ({ navigation }) => {
         });
     }
     return (
-        <View>
-            <Text>ChatScreen</Text>
-        </View>
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: 1,
+            }}
+        />
+
     )
 }
 
